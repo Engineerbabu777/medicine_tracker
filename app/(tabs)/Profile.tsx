121c7ac113cd04660fc5cc59b2656d314a143307
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-03 18:50:27 +0500

    add:/ tab navigation added

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,12 +1,13 @@
 
 
+
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Index() {
+export default function Profile() {
   return (
     <View>
-      <Text></Text>
+      <Text>Profile</Text>
      </View>
   );
 }
