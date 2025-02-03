b0108f2946ed254d470a4b6806c12875b022c467
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-03 19:04:41 +0500

    add:/ done with tab bar navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,50 @@
-
-
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { View, Text } from 'react-native';
-import Index from '.';
-import Profile from './Profile';
-import AddNew from './AddNew';
+import { Tabs } from "expo-router";
+import React from "react";
+import { View, Text } from "react-native";
+import Index from ".";
+import Profile from "./Profile";
+import AddNew from "./AddNew";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
   return (
     <>
-    <Tabs>
-        <Tabs.Screen name="index" />
-        <Tabs.Screen name="Profile"  />
-        <Tabs.Screen name="AddNew"  />
-    </Tabs>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarLabel: "Home",
+
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Profile"
+          options={{
+            tabBarLabel: "Add New",
+
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="plus-square" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="AddNew"
+          options={{
+            tabBarLabel: "Profile",
+
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="user" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
     </>
   );
 }
