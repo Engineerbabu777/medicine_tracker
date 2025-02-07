23f4d69862cabf14bd9cb59b57155dce6b09f96f
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-07 16:21:01 +0500

    add:/ empty state done for home page

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,12 +1,63 @@
-
-
-import React from 'react';
-import { View, Text } from 'react-native';
+import Colors from "@/constant/Colors";
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 export default function EmptyState() {
   return (
-    <View>
-      <Text>Empty state</Text>
-     </View>
+    <View
+      style={{
+        marginTop: 80,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={require("../assets/images/medicine.png")}
+        style={{
+          width: 120,
+          height: 120,
+        }}
+      />
+      <Text
+        style={{
+          fontSize: 35,
+          fontWeight: "bold",
+          marginTop: 30,
+        }}
+      >
+        No Medications!
+      </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          color: Colors.DARK_GRAY,
+          textAlign: "center",
+          marginTop: 20,
+        }}
+      >
+        You have 0 medications, Kindly setup a new one
+      </Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: Colors.PRIMARY,
+
+          marginTop: 30,
+          padding: 15,
+          borderRadius: 10,
+          width: "100%",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 17,
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          + Add New Medication
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 }
