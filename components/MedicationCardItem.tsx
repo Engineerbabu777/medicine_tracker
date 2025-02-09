fe2ed03bf97eac392bbb61a235305dc18dc5b3ee
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-09 19:33:54 +0500

    add:/ done with medication card items&& get medications from backend

---
+++ components/MedicationCardItem.tsx
@@ -0,0 +1,69 @@
+import Colors from "@/constant/Colors";
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+type Props = {
+  medicine: any;
+};
+
+export default function MedicationCardItem({ medicine }: Props) {
+  console.log({ medicine });
+  return (
+    <View style={styles.container} key={medicine.id}>
+      <View style={styles.subContainer}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{ uri: medicine.type.icon }}
+            style={{ width: 60, height: 60 }}
+          />
+        </View>
+        <View>
+          <Text style={{ fontSize: 22, fontWeight: "bold" }}>
+            {medicine.name}
+          </Text>
+          <Text style={{ fontSize: 17 }}>{medicine.whenToTake}</Text>
+          <Text style={{ color: "white" }}>
+            {medicine.dose} {medicine.type.name}
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          padding: 15,
+          borderRadius: 10,
+          alignItems: "center",
+        }}
+      >
+        <Ionicons name="timer-outline" size={24} color="black" />
+        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+          {medicine?.remainderTime}
+        </Text>
+      </View>
+    </View>
+  );
+}
+``
+const styles = StyleSheet.create({
+  imageContainer: {
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 15,
+    marginRight: 10,
+  },
+  subContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  container: {
+    padding: 10,
+    // backgroundColor: Colors.LIGHT_PRIMARY,
+    borderWidth: 1,
+    borderColor: Colors.LIGHT_GRAY_BORDER,
+    marginTop: 10,
+    borderRadius: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
--- components/MedicationList.tsx
+++ components/MedicationList.tsx
@@ -1,8 +1,68 @@
-import React, { useState } from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import { db } from "@/config/firebaseConfig";
+import Colors from "@/constant/Colors";
+import { getDateRangesToDisplay } from "@/service/ConvertDateTime";
+import { getLocalStorage } from "@/service/Storage";
+import { User } from "firebase/auth";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  FlatList,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import MedicationCardItem from "./MedicationCardItem";
 
 export default function MedicationList() {
-  const [medicationList, setMedicationList] = useState([]);
+  const [medicationList, setMedicationList] = useState<any>([]);
+  const [dateRangeList, setDateRangeList] = useState<any>([]);
+  const [selectedDate, setSelectedDate] = useState<any>(
+    moment().format("MM/DD/YYYY")
+  );
+
+  const getDateRangeList = () => {
+    const dates = getDateRangesToDisplay();
+    setDateRangeList(dates);
+    console.log(dates);
+  };
+
+  const getMedicationList = async (date: any) => {
+    const user: User | null = await getLocalStorage("user");
+
+    const medicationRef = collection(db, "medications");
+    const q = await query(
+      medicationRef,
+      where("userEmail", "==", user?.email),
+      where("dates", "array-contains", date)
+    );
+
+    const docs = await getDocs(q);
+    const medications = docs.docs.map((doc: DocumentData) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setMedicationList(medications);
+  };
+
+  // FETCH DATA BASED ON THE SELECTED DATE
+  useEffect(() => {
+    getMedicationList(selectedDate);
+  }, [selectedDate]);
+
+  useEffect(() => {
+    getDateRangeList();
+  }, []);
 
   return (
     <ScrollView
@@ -20,6 +80,83 @@ export default function MedicationList() {
           }}
         />
       </View>
+
+      <View
+        style={{
+          marginTop: 20,
+        }}
+      >
+        <FlatList
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={dateRangeList}
+          renderItem={({ item, index }: { item: any; index: number }) => (
+            <Pressable
+              style={[
+                styles.dateGroup,
+                {
+                  backgroundColor:
+                    item.formattedDate === selectedDate
+                      ? Colors.PRIMARY
+                      : Colors.LIGHT_GRAY_BORDER,
+                },
+              ]}
+              onPress={() => setSelectedDate(item?.formattedDate)}
+            >
+              <Text
+                style={[
+                  styles.day,
+                  {
+                    color:
+                      item.formattedDate === selectedDate ? "white" : "black",
+                  },
+                ]}
+              >
+                {item?.day}
+              </Text>
+              <Text
+                style={[
+                  styles.date,
+                  {
+                    color:
+                      item.formattedDate === selectedDate ? "white" : "black",
+                  },
+                ]}
+              >
+                {item?.date}
+              </Text>
+            </Pressable>
+          )}
+        />
+
+        <FlatList
+          data={medicationList}
+          renderItem={({ item }) => <MedicationCardItem medicine={item} />}
+        />
+      </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  dateGroup: {
+    padding: 15,
+    backgroundColor: Colors.LIGHT_GRAY_BORDER,
+    display: "flex",
+    marginRight: 15,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  day: {
+    fontSize: 20,
+  },
+  date: {
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  selectedDate: {
+    backgroundColor: Colors.PRIMARY,
+    color: "white",
+    borderRadius: 10,
+  },
+});
